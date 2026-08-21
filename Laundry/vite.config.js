8cbb90af55cccab7391d50463e0a3f88fb77fee9
mahimaanchra <mahimaanchra@gmail.com>
2026-08-21 16:38:55 +0530

    improved landing page

--- Laundry/vite.config.js
+++ Laundry/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    },
+    dedupe: ['react', 'react-dom'],
+  },
+  optimizeDeps: {
+    include: ['lucide-react', 'react', 'react-dom'],
+  },
+});
